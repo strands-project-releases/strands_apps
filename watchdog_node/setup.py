2a8af9eae18cef8895c9ebb6fb4359362dd320ed
STRANDS Jenkins <strands@hanheide.net>
2016-04-19 08:34:11 +0000

    Trimmed the branch to only the state_checker sub directory

--- watchdog_node/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['watchdog_node','watchdog_node.monitors','watchdog_node.actions'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
